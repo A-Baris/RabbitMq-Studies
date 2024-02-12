9ba62eb1302f40def383c93fc88413f6e5becd9a
A-Baris <barisemirdag@gmail.com>
2024-02-12 17:17:20 +0300

    Add project files.

---
+++ RabbitMq-Study.publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://xtnimwcx:XWIcS9Ni0pikXFBMkKVvzEsKEWZ4NB8N@whale.rmq.cloudamqp.com/xtnimwcx");
+
+using var connection = connectionFactory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("first-messages", true, false, false);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+
+    string message = $"This is message of {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "first-messages", null, messageBody);
+    Console.WriteLine($"Message is sent :{message} ");
+});
+
+Console.ReadLine();
+
+
---
+++ RabitMq-Study.Subcriber/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://xtnimwcx:XWIcS9Ni0pikXFBMkKVvzEsKEWZ4NB8N@whale.rmq.cloudamqp.com/xtnimwcx");
+
+using var connection = connectionFactory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+
+
+channel.BasicQos(0,1,false);
+var subcriber = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("first-messages",false,subcriber);
+
+subcriber.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(2000);
+    Console.WriteLine($"Received message: {message}");
+
+    channel.BasicAck(e.DeliveryTag, false);
+
+
+};
+Console.ReadLine();
